12ac93c7ab60576dc4b4300c403df698e55556dd
Marcus <jarvisandfriend@gmail.com>
2025-06-16 00:52:45 +0300

    @whisper

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Run this after cloning your project
+cd whisper.cpp && cmake -B build && cmake --build build --config Release
+# Download a model if not already there
+cd models
+./download-ggml-model.sh base.en && cd ..
+
