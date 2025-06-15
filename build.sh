67f4bb4a0e12acc8876f4279b84c3420870b679c
Marcus <jarvisandfriend@gmail.com>
2025-06-16 01:18:48 +0300

    @tesseract-stt

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#install ffmpeg
+sudo apt update && sudo apt install -y ffmpeg
 # Run this after cloning your project
 cd whisper.cpp && cmake -B build && cmake --build build --config Release
 # Download a model if not already there
